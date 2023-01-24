0d3309e2982098c3161f3de3ece6fee833be3bd7
Matheus Kamer <matheus_kamer@hotmail.com>
2023-01-24 19:07:53 -0400

    Terminando repository e controller dos contatos

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,22 +1,86 @@
+const ContactRepository = require('../repositories/ContactRepository');
+
 class ContactController {
   async index(request, response) {
-    response.send('Testando rota index');
+    const contacts = await ContactRepository.findAll();
+
+    response.json(contacts);
   }
 
   async show(request, response) {
-    response.send('Testando rota show');
+    const { id } = request.params;
+
+    const contact = await ContactRepository.findById(id);
+
+    if (!contact) {
+      return response.sendStatus(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
   }
 
   async store(request, response) {
-    response.send('Testando rota store');
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.sendStatus(404).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.sendStatus(404).json({ error: 'This email is already in use' });
+    }
+
+    const contact = await ContactRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async update(request, response) {
-    response.send('Testando rota update');
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactRepository.findById(id);
+
+    if (!contactExists) {
+      return response.sendStatus(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.sendStatus(404).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.sendStatus(404).json({ error: 'This email is already been taken' });
+    }
+
+    const contact = await ContactRepository.update({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
-    response.send('Testando rota delete');
+    const { id } = request.params;
+
+    const contactExists = await ContactRepository.findById(id);
+
+    if (!contactExists) {
+      return response.sendStatus(404).json({ error: 'User not found' });
+    }
+
+    await ContactRepository.delete(id);
+    response.sendStatus(204);
   }
 }
 
